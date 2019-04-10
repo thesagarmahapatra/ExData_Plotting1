5351562de0aba8618bae6a8e1858c249eceaf16d
thesagarmahapatra <sagar15mahapatra@gmail.com>
2019-04-10 15:17:30 +0530

    Added R code and statistical visualisation and data

---
+++ random.R
@@ -0,0 +1,33 @@
+#install.packages("jsonlite")
+library(jsonlite)
+#install.packages("httpuv")
+library(httpuv)
+#install.packages("httr")
+library(httr)
+
+# Can be github, linkedin etc depending on application
+oauth_endpoints("github")
+
+# Change based on what you 
+myapp <- oauth_app(appname = "Sagar_quiz1_cleaningData",
+                   key = "389e7ec7df859e7f7a25",
+                   secret = "71c56eed972475f13be121a42285ac0308458db0")
+
+# Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+# Take action on http error
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
